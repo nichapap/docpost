28ab0593d4850a48b850c18f868c1e1ecaf8b8f3
Pakpisit Pakhakorntanatorn <pakpisit.pakh@gmail.com>
2022-03-12 16:18:22 +0700

    update contact page

---
+++ lib/contacts_page.dart
@@ -0,0 +1,42 @@
+import 'package:flutter/material.dart';
+
+class Contacts extends StatelessWidget {
+  const Contacts({Key key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        body: Padding(
+          padding: const EdgeInsets.all(15),
+          child: Container(
+            alignment: Alignment.topLeft,
+            color: Colors.amber.shade100,
+            // padding: EdgeInsets.all(20),
+            child: Column(
+              children: [
+                Container(
+                  alignment: Alignment.topLeft,
+                  padding: const EdgeInsets.all(20),
+                  child: const Text(
+                    'Contact:',
+                    style: TextStyle(fontSize: 26, fontWeight: FontWeight.bold),
+                  ),
+                ),
+                Container(
+                  alignment: Alignment.topLeft,
+                  padding: const EdgeInsets.all(20),
+                  child: const Text(
+                  'Email - \n\n docpost@gmail.com \n docpost@hotmail.com',
+                  style: TextStyle(fontSize: 20),
+                  ),
+                
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
